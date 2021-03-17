2f6862053c7922822802044e84c3b93fcb017728
Halil Bagci <halilbagci75@hotmail.com>
2021-03-17 15:40:03 +0100

    Add testclass

--- src/test/java/com/netflix/api/service/GenreServiceTest.java
+++ src/test/java/com/netflix/api/service/GenreServiceTest.java
@@ -1,4 +1,38 @@
+package com.netflix.api.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GenreServiceTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getMovieViewsByGenre() {
+    }
+
+    @Test
+    void getMovieViewsByCompany() {
+    }
+
+    @Test
+    void getMovieViewsByCast() {
+    }
+
+    @Test
+    void get40MoviesByCast() {
+    }
+
+    @Test
+    void getMovieViewsByDecade() {
+    }
 }
